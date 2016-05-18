f63d06ea12915d053a7e34f44e43593bce6cd8d7
Rychard <guedes.rychard@gmail.com>
2016-05-18 15:54:56 -0300

    Using 595 properly

--- ass4/ass4.ino
+++ ass4/ass4.ino
@@ -1,5 +1,5 @@
 // x1 é o mais significativo
-const int data = 12, nclk = 8, rclk = 13, clr = 10, x1 = 14, x2 = 15, x3 = 16;
+const int data = 12, sclk = 13, ltch = 11, x1 = 14, x2 = 15, x3 = 16;
 unsigned long t1, t2, t3, t4;
 int count, mplx;
 
@@ -15,30 +15,40 @@ const byte bt[] = {B11111100, // 0
                    B11110110  // 9
 };
 
+const byte btp[] = {B11111101, // 0
+                    B01100001, // 1
+                    B11011011, // 2
+                    B11110011, // 3
+                    B01100111, // 4
+                    B10110111, // 5
+                    B10111111, // 6
+                    B11100001, // 7
+                    B11111111, // 8
+                    B11110111  // 9
+};
+
 void write7seg(byte b){
   for(int i = 0; i < 8; i++){
-    digitalWrite(rclk, LOW);
     digitalWrite(data, bitRead(b, i));
-    digitalWrite(nclk, LOW);
-    digitalWrite(nclk, HIGH);
+    digitalWrite(sclk, LOW);
+    digitalWrite(sclk, HIGH);
   }
-  digitalWrite(rclk, LOW);
-  digitalWrite(rclk, HIGH);
+  digitalWrite(ltch, LOW);
+  digitalWrite(ltch, HIGH);
+  digitalWrite(ltch, LOW);
 }
 
 void setup() {
   pinMode(x1, OUTPUT);
   pinMode(x2, OUTPUT);
   pinMode(x3, OUTPUT);
-  pinMode(clr, OUTPUT);
-  pinMode(nclk, OUTPUT);
-  pinMode(rclk, OUTPUT);
+  pinMode(sclk, OUTPUT);
+  pinMode(ltch, OUTPUT);
   pinMode(data, OUTPUT);
 
   digitalWrite(x1, HIGH);
   digitalWrite(x2, LOW);
   digitalWrite(x3, LOW);
-  digitalWrite(clr, HIGH);
 
   t1 = millis();
   t2 = millis();
@@ -58,31 +68,42 @@ void loop() {
     t1 = millis();
     if(mplx == 0){
       digitalWrite(x1, LOW);
-      digitalWrite(x2, LOW);
-      digitalWrite(x3, HIGH);
-      write7seg(bt[count%10]);
-      ++mplx;
-    } else if (mplx == 1) {
-      digitalWrite(x1, LOW);
       digitalWrite(x2, HIGH);
       digitalWrite(x3, LOW);
-      write7seg(bt[(count/10)%10]);
+      if(count < 999){
+        write7seg(bt[count%10]);  
+      } else {
+        write7seg(bt[(count/10)%10]); 
+      }
       ++mplx;
-    } else {
+    } else if (mplx == 1) {
       digitalWrite(x1, HIGH);
       digitalWrite(x2, LOW);
       digitalWrite(x3, LOW);
-      write7seg(bt[(count/100)]);
+      if(count < 999){
+        write7seg(bt[(count/10)%10]);
+      } else {
+        write7seg(bt[(count/100)%10]);
+      }
+      ++mplx;
+    } else {
+      digitalWrite(x1, LOW);
+      digitalWrite(x2, LOW);
+      digitalWrite(x3, HIGH);
+      if(count < 999){
+        write7seg(bt[(count/100)]);
+      } else {
+        write7seg(btp[count/1000]);
+      }
       mplx = 0;
-      
     }
   }
 
   t4 = millis();
-  if(t4 - t3 > 20){
+  if(t4 - t3 > 10){
     t3 = millis();
     count++;
-    if(count == 1000)
+    if(count == 9999)
       count = 0;
   }
 
