edbd1d0d4f4a3843e363926aa902fcdae0b93677
Rychard <guedes.rychard@gmail.com>
2016-05-28 13:28:13 -0300

    Keyboard and bhaskara function completed

--- ass5/ass5.ino
+++ ass5/ass5.ino
@@ -1,10 +1,14 @@
-const int ncol = 4, nlin = 4;                       // Quantidade de linhas e colunas da matriz de teclas
-const int col[] = {A0, A1, A2, 7}, lin[] = {11, 10, A4, A5};   // Vetores de teclas
+#include <LiquidCrystal.h>
 
-boolean neg = true, flt = true;
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+
+const int ncol = 4, nlin = 4;                       // Quantidade de linhas e colunas da matriz de teclas
+const int col[] = {14, 15, 16, 17}, lin[] = {5, 4, 3, 2};   // Vetores de teclas
+
+boolean neg = true, flt = true, show_msg = true;
 char tecla;
-int coef_n = 0;
-float num_f, coef[3] = {0, 0, 0};
+int coef_n = 0, qtd_raizes;
+float num_f, coef[3] = {0, 0, 0}, x1, x2;
 String num_s = "";
  
 
@@ -41,15 +45,24 @@ char getKey(){
   return key;
 }
 
-String getNum(){
-  
-  
+int bhaskara(float a, float b, float c){
 
-  
+  float delta = b*b - 4*a*c;
+
+  if(delta < 0)
+    return 0;
+  else if (delta == 0){
+    x1 = x2 = -b/(2*a);
+    return 1;
+  } else {
+    x1 = (-b + sqrt(delta))/(2*a);
+    x2 = (-b - sqrt(delta))/(2*a);
+    return 2;
+  }
 }
 
 void setup() {
-  
+ 
   for(int i = 0; i < ncol; i++){
     pinMode(col[i], INPUT);
     digitalWrite(col[i], HIGH);
@@ -59,21 +72,36 @@ void setup() {
 
   Serial.begin(9600);
   Serial.println("Pinos setados");
-  
+
+  lcd.begin(16, 2);
+  lcd.print("EQUACAO 2 GRAU");
+  delay(1000);
+  lcd.clear();
 }
 
 void loop() {
   tecla = getKey();
 
+  
   if(tecla != 0){
     Serial.print(tecla);
+    
     if(tecla == '='){
+      
       coef[coef_n] = num_s.toFloat();
       Serial.println();
       Serial.print("Numero lido: ");
       Serial.println(coef[coef_n++]);
+      Serial.println("coef_n: ");
+      Serial.println(coef_n);
       num_s = "";
-      flt = true; 
+
+      if(coef_n == 1)
+        Serial.print("Digite B: ");
+      else if (coef_n == 2)
+        Serial.print("Digite C: ");
+        
+      flt = true;
     } else if (tecla == '.'){
       if(flt){
         num_s += tecla;
@@ -89,13 +117,22 @@ void loop() {
       num_s += tecla;
     } 
   }
-  if (coef_n == 3)
+  
+  if (coef_n == 3){
     coef_n = 0;
-  /*(tecla = getNum(); //getKey();
 
-  if(tecla != 0){
-    Serial.print("Tecla lida: ");
-    Serial.println(tecla);
-  }*/
+    qtd_raizes = bhaskara(coef[0], coef[1], coef[2]);
+
+    if(qtd_raizes == 0){
+      Serial.println("Não existem raizes reais");
+    } else {
+      Serial.println("Existem raizes reais: ");
+      Serial.println(x1);
+      Serial.println(x2);
+    }
+    Serial.println();
+    delay(200);
+    setup();
+  }
   
 }
